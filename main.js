b0a6382d79472a79648d45cfb551ab7bd31fa405
DenisPapushaJava <denis-papusha@rambler.ru>
2021-10-02 08:08:24 +0300

    new

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-    $('.burger-menu').click(function(event){
+$(document).ready(function () {
+    $('.burger-menu').click(function (event) {
         $('.burger-menu, .bg-lists').toggleClass('active');
     });
-});
+
+    let btnTop = $('#btn-top');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 200) {
+            btnTop.addClass('show');
+        }
+        else {
+            btnTop.removeClass('show');
+        }
+    });
+    btnTop.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop:0}, '300');
+    })});
